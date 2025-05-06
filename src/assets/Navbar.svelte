e02293289bcf62f148b86ebfa7e4db88e71a8b0b
Jayv2017 <75362566+Jayv2017@users.noreply.github.com>
2025-05-06 13:51:10 +0530

    working on the navbar

---
+++ src/assets/Navbar.svelte
@@ -0,0 +1,10 @@
+<nav class="sticky top-0 bg-dark px-6 py-3 shadow-md flex justify-between items-center">
+  <div class="text-xl font-alconica text-white p-2">yuvoa</div>
+  <div class="space-x-4 text-sm">
+    <a href="#about" class="text-light relative group px-6 py-2">
+      log in/sign up
+      <!-- Hover effect line -->
+      <span class="absolute left-0 bottom-0 h-0.5 w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
+    </a>
+  </div>
+</nav>
---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Pathway Extreme"', "sans-serif"],
+        alconica: ["Alconica", "sans-serif"],
+      },
+      colors: {
+        dark: "#02050D",
+        light: "#E5E5E5",
+      },
+    },
+  },
+  plugins: [],
+};
